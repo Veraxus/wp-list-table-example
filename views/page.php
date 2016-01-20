17ce3995bffaea89f049766c124a7098bee8b406
Gary Jones <gary@gamajo.com>
2016-01-20 09:35:38 +0000

    View: Remove unused icon markup
    Icons are no longer used in WP admin pages.

--- views/page.php
+++ views/page.php
@@ -1,6 +1,4 @@
 <div class="wrap">
-
-	<div id="icon-users" class="icon32"><br/></div>
 	<h2>List Table Test</h2>
 
 	<div style="background:#ECECEC;border:1px solid #CCC;padding:0 10px;margin-top:5px;border-radius:5px;-moz-border-radius:5px;-webkit-border-radius:5px;">
