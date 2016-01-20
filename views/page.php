73e07f3fd4c6052c9eaf027b9fcc8879f6558684
Gary Jones <gary@gamajo.com>
2016-01-20 10:01:14 +0000

    View: Add Developer code reference link
    The Codex will eventually be replaced, so may as well add in a link to its replacement now.

--- views/page.php
+++ views/page.php
@@ -2,9 +2,9 @@
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 
 	<div style="background:#ececec;border:1px solid #ccc;padding:0 10px;margin-top:5px;border-radius:5px;">
-		<p>This page demonstrates the use of the <code><a href="http://codex.wordpress.org/Class_Reference/WP_List_Table" target="_blank">WP_List_Table</a></code> class in plugins.</p>
+		<p>This page demonstrates the use of the <code>WP_List_Table</code> class in plugins.</p>
 		<p>For a detailed explanation of using the <code>WP_List_Table</code> class in your own plugins, simply open <code>class-tt-example-list-table.php</code> in the PHP editor of your choice.</p>
-		<p>Additional class details are available on the WordPress Codex.</p>
+		<p>Additional class details are available on the <a href="http://codex.wordpress.org/Class_Reference/WP_List_Table" target="_blank">WordPress Codex</a> or <a href="https://developer.wordpress.org/reference/classes/WP_List_Table/" target="_blank">Developer Code Reference</a>.</p>
 	</div>
 
 	<!-- Forms are NOT created automatically, so you need to wrap the table in one to use features like bulk actions -->
