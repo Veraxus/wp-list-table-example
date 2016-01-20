896dba5b80cecc6b68fcfc14c7dfea7f622cda0a
Gary Jones <gary@gamajo.com>
2016-01-20 09:19:04 +0000

    Loading the base class: Fix up
    * Check if `ABSPATH` is defined. This is typical in plugin files that have function calls in the global scope, to protect against calls to the file directly. Here, it also serves to ensure that ABSPATH is actually defined before referencing it for the purposes of loading the base list table class.
    * Code standards and documentation.

--- list-table-example.php
+++ list-table-example.php
@@ -42,10 +42,14 @@
  * an update is released, your changes will be lost!
  * ========================================================================== */
 
+// Abort if this file is accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-
-/*************************** LOAD THE BASE CLASS *******************************
- *******************************************************************************
+/*
+ * Load the base class.
+ *
  * The WP_List_Table class isn't automatically available to plugins, so we need
  * to check if it's available and load it if necessary. In this tutorial, we are
  * going to use the WP_List_Table class directly from WordPress core.
@@ -64,8 +68,8 @@
  * Since I will be keeping this tutorial up-to-date for the foreseeable future,
  * I am going to work with the copy of the class provided in WordPress core.
  */
-if(!class_exists('WP_List_Table')){
-    require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+if ( ! class_exists( 'WP_List_Table' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
 }
 
 // Require the custom list table class.
