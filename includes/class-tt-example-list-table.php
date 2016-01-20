5560d46ce85c9bd7dc43ed8d005628de27305aba
Gary Jones <gary@gamajo.com>
2016-01-20 13:53:54 +0000

    Class: Fix up prepare_items()
    Code standards and documentation.

--- includes/class-tt-example-list-table.php
+++ includes/class-tt-example-list-table.php
@@ -302,7 +302,7 @@ class TT_Example_List_Table extends WP_List_Table {
 	 * clean and organized.
 	 *
 	 * @see $this->prepare_items()
-	 **************************************************************************/
+	 */
 	protected function process_bulk_action() {
 		// Detect when a bulk action is being triggered.
 		if ( 'delete' === $this->current_action() ) {
@@ -310,14 +310,16 @@ class TT_Example_List_Table extends WP_List_Table {
 		}
 	}
 
-	/** ************************************************************************
+	/**
+	 * Prepares the list of items for displaying.
+	 *
 	 * REQUIRED! This is where you prepare your data for display. This method will
 	 * usually be used to query the database, sort and filter the data, and generally
 	 * get it ready to be displayed. At a minimum, we should set $this->items and
 	 * $this->set_pagination_args(), although the following properties and methods
-	 * are frequently interacted with here...
+	 * are frequently interacted with here.
 	 *
-	 * @global WPDB $wpdb
+	 * @global wpdb $wpdb
 	 * @uses $this->_column_headers
 	 * @uses $this->items
 	 * @uses $this->get_columns()
@@ -328,13 +330,12 @@ class TT_Example_List_Table extends WP_List_Table {
 	function prepare_items() {
 		global $wpdb; //This is used only if making any database queries
 
-		/**
+		/*
 		 * First, lets decide how many records per page to show
 		 */
 		$per_page = 5;
 
-
-		/**
+		/*
 		 * REQUIRED. Now we need to define our column headers. This includes a complete
 		 * array of columns to be displayed (slugs & titles), a list of columns
 		 * to keep hidden, and a list of columns that are sortable. Each of these
@@ -345,15 +346,13 @@ class TT_Example_List_Table extends WP_List_Table {
 		$hidden = array();
 		$sortable = $this->get_sortable_columns();
 
-
-		/**
+		/*
 		 * REQUIRED. Finally, we build an array to be used by the class for column
 		 * headers. The $this->_column_headers property takes an array which contains
-		 * 3 other arrays. One for all columns, one for hidden columns, and one
+		 * three other arrays. One for all columns, one for hidden columns, and one
 		 * for sortable columns.
 		 */
-		$this->_column_headers = array($columns, $hidden, $sortable);
-
+		$this->_column_headers = array( $columns, $hidden, $sortable );
 
 		/**
 		 * Optional. You can handle your bulk actions however you see fit. In this
@@ -361,8 +360,7 @@ class TT_Example_List_Table extends WP_List_Table {
 		 */
 		$this->process_bulk_action();
 
-
-		/**
+		/*
 		 * Instead of querying a database, we're going to fetch the example data
 		 * property we created for use in this plugin. This makes this example
 		 * package slightly different than one you might build on your own. In
@@ -373,8 +371,7 @@ class TT_Example_List_Table extends WP_List_Table {
 		 */
 		$data = $this->example_data;
 
-
-		/**
+		/*
 		 * This checks for sorting input and sorts the data in our array accordingly.
 		 *
 		 * In a real-world situation involving a database, you would probably want
@@ -382,68 +379,73 @@ class TT_Example_List_Table extends WP_List_Table {
 		 * to a custom query. The returned data will be pre-sorted, and this array
 		 * sorting technique would be unnecessary.
 		 */
-		function usort_reorder($a,$b){
-			$orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'title'; //If no sort, default to title
-			$order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; //If no order, default to asc
-			$result = strcmp($a[$orderby], $b[$orderby]); //Determine sort order
-			return ($order==='asc') ? $result : -$result; //Send final sort direction to usort
-		}
-		usort($data, 'usort_reorder');
+		usort( $data, array( $this, 'usort_reorder' ) );
 
-
-		/***********************************************************************
-		 * ---------------------------------------------------------------------
-		 * vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-		 *
+		/*
+		 * ***********************************************************************
 		 * In a real-world situation, this is where you would place your query.
 		 *
 		 * For information on making queries in WordPress, see this Codex entry:
 		 * http://codex.wordpress.org/Class_Reference/wpdb
-		 *
-		 * ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-		 * ---------------------------------------------------------------------
-		 **********************************************************************/
+		 * ***********************************************************************
+		 */
 
-
-		/**
+		/*
 		 * REQUIRED for pagination. Let's figure out what page the user is currently
 		 * looking at. We'll need this later, so you should always include it in
 		 * your own package classes.
 		 */
 		$current_page = $this->get_pagenum();
 
-		/**
+		/*
 		 * REQUIRED for pagination. Let's check how many items are in our data array.
 		 * In real-world use, this would be the total number of items in your database,
 		 * without filtering. We'll need this later, so you should always include it
 		 * in your own package classes.
 		 */
-		$total_items = count($data);
+		$total_items = count( $data );
 
-
-		/**
+		/*
 		 * The WP_List_Table class does not handle pagination for us, so we need
 		 * to ensure that the data is trimmed to only the current page. We can use
-		 * array_slice() to
+		 * array_slice() to do that.
 		 */
-		$data = array_slice($data,(($current_page-1)*$per_page),$per_page);
+		$data = array_slice( $data,( ( $current_page - 1 ) * $per_page ), $per_page );
 
-
-
-		/**
+		/*
 		 * REQUIRED. Now we can add our *sorted* data to the items property, where
 		 * it can be used by the rest of the class.
 		 */
 		$this->items = $data;
 
-
 		/**
 		 * REQUIRED. We also have to register our pagination options & calculations.
 		 */
 		$this->set_pagination_args( array(
-			'total_items' => $total_items,                  //WE have to calculate the total number of items
-			'per_page'    => $per_page,                     //WE have to determine how many items to show on a page
-			'total_pages' => ceil($total_items/$per_page)   //WE have to calculate the total number of pages
+			'total_items' => $total_items,                       // WE have to calculate the total number of items.
+			'per_page'    => $per_page,                          // WE have to determine how many items to show on a page.
+			'total_pages' => ceil( $total_items / $per_page ),   // WE have to calculate the total number of pages.
 		) );
+	}
+
+	/**
+	 * Callback to allow sorting of example data.
+	 *
+	 * @param string $a First value.
+	 * @param string $b Second value.
+	 *
+	 * @return int
+	 */
+	protected function usort_reorder( $a, $b ) {
+		// If no sort, default to title.
+		$orderby = ! empty( $_REQUEST['orderby'] ) ? wp_unslash( $_REQUEST['orderby'] ) : 'title'; // WPCS: Input var ok.
+
+		// If no order, default to asc.
+		$order = ! empty( $_REQUEST['order'] ) ? wp_unslash( $_REQUEST['order'] ) : 'asc'; // WPCS: Input var ok.
+
+		// Determine sort order.
+		$result = strcmp( $a[ $orderby ], $b[ $orderby ] );
+
+		return ('asc' === $order ) ? $result : -$result;
 	}
 }
